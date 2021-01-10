f476c04832469fe5defe86e1dc27b2328e6d4ec2
cdsalty <soltiscd@gmail.com>
2021-01-10 16:57:39 -0500

    adding the basics of yup and error validation

--- src/components/FormikForm.js
+++ src/components/FormikForm.js
@@ -1,5 +1,13 @@
+import * as Yup from "yup";
 import { Formik } from "formik";
 import "./formikForm.css";
+
+const FormikYup_Schema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required(),
+  password: Yup.string().required()
+});
 
 const FormikForm = () => {
   return (
@@ -10,29 +18,44 @@ const FormikForm = () => {
           email: "",
           password: ""
         }}
+        validationSchema={FormikYup_Schema}
         onSubmit={data => {
           console.log(data);
         }}
       >
         {/* 2. Pass a function to access props or destructure out the props you want */}
-        {({ handleSubmit, handleChange, handleBlur, values }) => {
+        {({ handleSubmit, handleChange, handleBlur, values, errors }) => {
           // return "test";
-          // console.log(values); // to see everything change as I type
+          console.log(errors); // to see everything change as I type
           return (
-            <form onSubmit={handleSubmit}>
-              <div className="input_wrapper">
-                <label htmlFor="">Email:</label>
-                <input
-                  type="text"
-                  name={"email"} // if I passed "email2", the value woould create and assign the value to a new property value of email2
-                  value={values.email} // e.target.value or the value of the particular input field
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  placeholder={"Email..."}
-                />
-              </div>
-              <button type={"submit"}>Submit</button>
-            </form>
+            <div className="input_container">
+              <form onSubmit={handleSubmit}>
+                <div className="input_wrapper">
+                  <label htmlFor="email">Email: </label>
+                  <input
+                    type="text"
+                    name={"email"} // if I passed "email2", the value woould create and assign the value to a new property value of email2
+                    value={values.email} // e.target.value or the value of the particular input field
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder={"Email..."}
+                  />
+                </div>
+                <div className="input_wrapper">
+                  <label password="password">Password: </label>
+                  <input
+                    type="password"
+                    name={"password"} // if I passed "email2", the value woould create and assign the value to a new property value of email2
+                    autoComplete="on" // to remove the alert in the console about this suggestion
+                    value={values.password} // e.target.value or the value of the particular input field
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder={"Password"}
+                  />
+                </div>
+                <button type={"submit"}>Submit</button>
+              </form>
+            </div>
           );
         }}
       </Formik>
